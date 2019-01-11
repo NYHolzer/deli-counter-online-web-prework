86d27748c921a2154c7179e80a0e44c15c0d5a7a
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-11 02:06:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+
+def line (katz_deli) 
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else
+    x = katz_deli.map.with_index(1) do |name, index|
+      " #{index}. #{name}"
+    end 
+    puts "The line is currently:" << x.to_s
+  end
+end
